2dea83918cfcc09d2961cf123f9975355cd53ef0
Gabor Marton <gabor.marton@prezi.com>
2014-12-13 15:44:36 +0100

    Added Result struct for the matcher.

--- FriendStats.cpp
+++ FriendStats.cpp
@@ -13,26 +13,33 @@ using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
 
-StatementMatcher LoopMatcher =
-  forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(
-    hasInitializer(integerLiteral(equals(0)))))))).bind("forLoop");
-
-class LoopPrinter : public MatchFinder::MatchCallback {
-public :
-  virtual void run(const MatchFinder::MatchResult &Result) {
-    if (const ForStmt *FS = Result.Nodes.getNodeAs<clang::ForStmt>("forLoop"))
-      FS->dump();
-  }
+struct MyResult {
+  int friendClassCount = 0;
+  int friendFuncCount = 0;
 };
 
-auto FriendMatcher =
-  friendDecl().bind("friend");
+auto FriendMatcher = friendDecl().bind("friend");
 class FriendPrinter : public MatchFinder::MatchCallback {
-public :
+  MyResult result;
+
+public:
   virtual void run(const MatchFinder::MatchResult &Result) {
-    if (const FriendDecl *FD = Result.Nodes.getNodeAs<clang::FriendDecl>("friend"))
-      FD->dump();
+    if (const FriendDecl *FD =
+            Result.Nodes.getNodeAs<clang::FriendDecl>("friend")) {
+      // FD->dump();
+      if (FD->getFriendType()) {
+        llvm::outs() << "Class/Struct\n";
+        ++result.friendClassCount;
+      } else {
+        llvm::outs() << "Function\n";
+        ++result.friendFuncCount;
+      }
+      FD->getLocation().dump(*Result.SourceManager);
+      llvm::outs() << "\n";
+      llvm::outs().flush();
+    }
   }
+  const MyResult &getResult() const { return result; }
 };
 
 // Apply a custom category to all command-line options so that they are the
@@ -56,5 +63,32 @@ int main(int argc, const char **argv) {
   MatchFinder Finder;
   Finder.addMatcher(FriendMatcher, &Printer);
 
-  return Tool.run(newFrontendActionFactory(&Finder).get());
+  auto ret = Tool.run(newFrontendActionFactory(&Finder).get());
+  llvm::outs() << "MyResult.Class: " << Printer.getResult().friendClassCount
+               << "\n";
+  llvm::outs() << "MyResult.Func: " << Printer.getResult().friendFuncCount
+               << "\n";
+  return ret;
 }
+
+// int main() {
+// std::string error;
+// std::vector<std::string> sources{"test1.cpp"};
+// auto compDb = CompilationDatabase::autoDetectFromDirectory("./test", error);
+// if (!error.empty()) {
+// llvm::outs() << "ERROR: ";
+// llvm::outs() << error << "\n";
+//}
+// ClangTool Tool(*compDb, sources);
+
+// FriendPrinter Printer;
+// MatchFinder Finder;
+// Finder.addMatcher(FriendMatcher, &Printer);
+
+// auto ret = Tool.run(newFrontendActionFactory(&Finder).get());
+// llvm::outs() << "MyResult.Class: " << Printer.getResult().friendClassCount
+//<< "\n";
+// llvm::outs() << "MyResult.Func: " << Printer.getResult().friendFuncCount
+//<< "\n";
+// return ret;
+//}
