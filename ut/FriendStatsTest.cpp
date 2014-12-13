cac619e1dabdaa13e957b1865533c7c567c82e03
Gabor Marton <gabor.marton@prezi.com>
2014-12-13 18:00:29 +0100

    Added ClassCount test.

--- ut/FriendStatsTest.cpp
+++ ut/FriendStatsTest.cpp
@@ -10,15 +10,25 @@ using namespace clang::tooling;
 using namespace llvm;
 using namespace clang;
 
-TEST(First, first) {
-  std::string error;
-  std::vector<std::string> sources{"test1.cpp"};
-  auto compDb = CompilationDatabase::autoDetectFromDirectory("./test", error);
-  if (!error.empty()) {
-    llvm::outs() << "ERROR: ";
-    llvm::outs() << error << "\n";
-  }
-  ClangTool Tool(*compDb, sources);
+TEST(FriendStats, ClassCount) {
+  // The directory used is not important since the path gets mapped to a virtual
+  // file anyway. What is important is that we have an absolute path with which
+  // to use with mapVirtualFile().
+  SmallString<128> CurrentDir;
+  std::error_code EC = llvm::sys::fs::current_path(CurrentDir);
+  assert(!EC);
+  (void)EC;
+
+  SmallString<128> FileA = CurrentDir;
+  llvm::sys::path::append(FileA, "a.cc");
+
+  tooling::FixedCompilationDatabase Compilations(CurrentDir.str(),
+                                                 std::vector<std::string>());
+  std::vector<std::string> Sources;
+  Sources.push_back(FileA.str());
+  tooling::ClangTool Tool(Compilations, Sources);
+
+  Tool.mapVirtualFile(FileA, "class A { friend class B; }; class B {};");
 
   FriendPrinter Printer;
   MatchFinder Finder;
@@ -29,5 +39,8 @@ TEST(First, first) {
                << "\n";
   llvm::outs() << "MyResult.Func: " << Printer.getResult().friendFuncCount
                << "\n";
+  auto res = Printer.getResult();
+  EXPECT_EQ(res.friendClassCount, 1);
+  EXPECT_EQ(res.friendFuncCount, 0);
 }
 
