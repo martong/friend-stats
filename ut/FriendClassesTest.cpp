d92f2669ad7942e67a5322bca8a1b8a34efe87e6
Gabor Marton <gabor.marton@prezi.com>
2015-01-18 15:15:04 +0100

    Added tests TemplateHostClass and TemplateFriendClass.

--- ut/FriendClassesTest.cpp
+++ ut/FriendClassesTest.cpp
@@ -108,3 +108,62 @@ class C {
   EXPECT_EQ(fr.usedPrivateVarsCount, 1);
   EXPECT_EQ(fr.parentPrivateVarsCount, 3);
 }
+
+TEST_F(FriendClassesStats, TemplateHostClass) {
+  Tool->mapVirtualFile(FileA,
+                       R"phi(
+template <class T>
+class A {
+  int a = 0;
+  int b;
+  int c;
+  friend class B;
+};
+class B {
+  void func(A<int> &a) {
+    a.a = 1;
+    a.b = 2;
+  }
+};
+    )phi");
+  Tool->run(newFrontendActionFactory(&Finder).get());
+  auto res = Handler.getResult();
+  ASSERT_EQ(res.friendClassCount, 1);
+  ASSERT_EQ(res.ClassResults.size(), std::size_t{1});
+  ASSERT_EQ(res.ClassResults.begin()->second.memberFuncResults.size(),
+            std::size_t{1});
+  auto fr = getFirstMemberFuncResult(res);
+  EXPECT_EQ(fr.usedPrivateVarsCount, 2);
+  EXPECT_EQ(fr.parentPrivateVarsCount, 3);
+}
+
+TEST_F(FriendClassesStats, TemplateFriendClass) {
+  Tool->mapVirtualFile(FileA,
+                       R"phi(
+class A {
+  int a = 0;
+  int b;
+  int c;
+  template <class T>
+  friend class B;
+};
+template <class T>
+class B {
+  void func(A &a) {
+    a.a = 1;
+    a.b = 2;
+  }
+};
+template class B<int>;
+    )phi");
+  Tool->run(newFrontendActionFactory(&Finder).get());
+  auto res = Handler.getResult();
+  ASSERT_EQ(res.friendClassCount, 1);
+  ASSERT_EQ(res.ClassResults.size(), std::size_t{1});
+  ASSERT_EQ(res.ClassResults.begin()->second.memberFuncResults.size(),
+            std::size_t{1});
+  auto fr = getFirstMemberFuncResult(res);
+  EXPECT_EQ(fr.usedPrivateVarsCount, 2);
+  EXPECT_EQ(fr.parentPrivateVarsCount, 3);
+}
+
