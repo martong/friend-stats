3cd5a80767f3d5c437489066f314eee7595b448c
Gabor Marton <gabor.marton@prezi.com>
2015-02-22 18:10:04 +0100

    Moved Result to Data.hpp .

---
+++ Data.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <clang/Basic/SourceLocation.h>
+
+// FIXME
+using namespace clang;
+
+// Holds the number of private or protected variables, methods, types
+// in a class.
+struct ClassCounts {
+  int privateVarsCount = 0;
+  int privateMethodsCount = 0;
+  int privateTypesCount = 0;
+};
+
+struct Result {
+
+  // Number of friend decls which refer to a class or class template
+  int friendClassDeclCount = 0;
+  // Number of friend decls which refer to a function or function template
+  int friendFuncDeclCount = 0;
+
+  struct FuncResult {
+
+    std::string diagName;
+    SourceLocation friendDeclLoc; // The location of the friend declaration
+    std::string friendDeclLocStr;
+    SourceLocation defLoc; // The location of the definition of the friend
+    std::string defLocStr;
+
+    // Below the static variables and static methods are counted in as well.
+    // The number of used variables in this (friend) function
+    int usedPrivateVarsCount = 0;
+    // The number of priv/protected variables
+    // in this (friend) function's referred class.
+    int parentPrivateVarsCount = 0;
+    // The number of used methods in this (friend) function
+    int usedPrivateMethodsCount = 0;
+    // The number of priv/protected methods
+    // in this (friend) function's referred class.
+    int parentPrivateMethodsCount = 0;
+
+    struct Types {
+      // The number of used types in this (friend) function
+      int usedPrivateCount = 0;
+      // The number of priv/protected types
+      // in this (friend) function's referred class.
+      int parentPrivateCount = 0;
+    } types;
+  };
+
+  // Each friend funciton declaration might have it's connected function
+  // definition.
+  // We collect statistics only on friend functions and friend function
+  // templates with body (i.e if they have the definition provided).
+  // Each friend function template could have different specializations with
+  // their own definition.
+  using FriendDeclId = std::string;
+  using FunctionTemplateInstantiationId = std::string;
+  using FuncResultsForFriendDecl =
+      std::map<FunctionTemplateInstantiationId, FuncResult>;
+  std::map<FriendDeclId, FuncResultsForFriendDecl> FuncResults;
+
+  struct ClassResult {
+    std::string diagName;
+    FuncResultsForFriendDecl memberFuncResults;
+  };
+  // Each friend class template could have different specializations or
+  // instantiations. We handle one non-template class exactly the same as it was
+  // an instantiation/specialization of a class template. We collect statistics
+  // from every member functions of such records(class template inst/spec or
+  // non-template class). Also we collect stats from member function templates.
+  // We do this exactly the same as we do it with direct friend functions and
+  // function templates.
+  using ClassTemplateInstantiationId = std::string;
+  using ClassResultsForFriendDecl =
+      std::map<ClassTemplateInstantiationId, ClassResult>;
+  std::map<FriendDeclId, ClassResultsForFriendDecl> ClassResults;
+};
+
--- FriendStats.hpp
+++ FriendStats.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <set>
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/TypeVisitor.h"
-#include <map>
-#include <set>
+
+#include "Data.hpp"
+
+// FIXME
+using namespace clang;
+using namespace clang::ast_matchers;
 
 // TODO this should be a command line parameter or something similar, but
 // definitely should not be in vcs.
@@ -14,82 +19,6 @@ const bool debug = false;
 inline llvm::raw_ostream &debug_stream() {
   return debug ? llvm::outs() : llvm::nulls();
 }
-
-using namespace clang;
-using namespace clang::ast_matchers;
-
-// Holds the number of private or protected variables, methods, types
-// in a class.
-struct ClassCounts {
-  int privateVarsCount = 0;
-  int privateMethodsCount = 0;
-  int privateTypesCount = 0;
-};
-
-struct Result {
-
-  // Number of friend decls which refer to a class or class template
-  int friendClassDeclCount = 0;
-  // Number of friend decls which refer to a function or function template
-  int friendFuncDeclCount = 0;
-
-  struct FuncResult {
-
-    std::string diagName;
-    SourceLocation friendDeclLoc; // The location of the friend declaration
-    std::string friendDeclLocStr;
-    SourceLocation defLoc; // The location of the definition of the friend
-    std::string defLocStr;
-
-    // Below the static variables and static methods are counted in as well.
-    // The number of used variables in this (friend) function
-    int usedPrivateVarsCount = 0;
-    // The number of priv/protected variables
-    // in this (friend) function's referred class.
-    int parentPrivateVarsCount = 0;
-    // The number of used methods in this (friend) function
-    int usedPrivateMethodsCount = 0;
-    // The number of priv/protected methods
-    // in this (friend) function's referred class.
-    int parentPrivateMethodsCount = 0;
-
-    struct Types {
-      // The number of used types in this (friend) function
-      int usedPrivateCount = 0;
-      // The number of priv/protected types
-      // in this (friend) function's referred class.
-      int parentPrivateCount = 0;
-    } types;
-  };
-
-  // Each friend funciton declaration might have it's connected function
-  // definition.
-  // We collect statistics only on friend functions and friend function
-  // templates with body (i.e if they have the definition provided).
-  // Each friend function template could have different specializations with
-  // their own definition.
-  using FriendDeclId = std::string;
-  using FunctionTemplateInstantiationId = std::string;
-  using FuncResultsForFriendDecl =
-      std::map<FunctionTemplateInstantiationId, FuncResult>;
-  std::map<FriendDeclId, FuncResultsForFriendDecl> FuncResults;
-
-  struct ClassResult {
-    std::string diagName;
-    FuncResultsForFriendDecl memberFuncResults;
-  };
-  // Each friend class template could have different specializations or
-  // instantiations. We handle one non-template class exactly the same as it was
-  // an instantiation/specialization of a class template. We collect statistics
-  // from every member functions of such records(class template inst/spec or
-  // non-template class). Also we collect stats from member function templates.
-  // We do this exactly the same as we do it with direct friend functions and
-  // function templates.
-  using ClassTemplateInstantiationId = std::string;
-  using ClassResultsForFriendDecl =
-      std::map<ClassTemplateInstantiationId, ClassResult>;
-  std::map<FriendDeclId, ClassResultsForFriendDecl> ClassResults;
-};
 
 template <typename T> bool privOrProt(const T *x) {
   return x->getAccess() == AS_private || x->getAccess() == AS_protected;
